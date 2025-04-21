b91171ff72d599a6611668fda3544b4c0cced184
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-04-21 19:47:37 +0300

    MementoTry3

--- src/Models/OrderHistory.java
+++ src/Models/OrderHistory.java
@@ -3,15 +3,19 @@ package Models;
 import java.util.Stack;
 
 public class OrderHistory {
+    private Order originator;
     private final Stack<OrderMemento> history = new Stack<>();
 
     public void save(OrderMemento memento) {
+        originator = memento.getState();
         history.push(memento);
     }
 
     public OrderMemento undo() {
         if (!history.isEmpty()) {
-            return history.pop();
+            OrderMemento m = history.pop();
+            originator = m.getState();
+            return m;
         }
         return null;
     }
