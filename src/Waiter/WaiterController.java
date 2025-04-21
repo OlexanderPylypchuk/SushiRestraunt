6646d1191e7c4ff97bebf1943f7e19bcd7dfdb34
OlexanderPylypchuk <sashylichka2016@gmail.com>
2025-04-21 15:33:36 +0300

    ComandTry@

--- src/Waiter/WaiterController.java
+++ src/Waiter/WaiterController.java
@@ -4,9 +4,10 @@ import Waiter.Interface.WaiterCommand;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class WaiterController {
-    private final List<WaiterCommand> queue = new ArrayList<>();
+    private final Stack<WaiterCommand> queue = new Stack<WaiterCommand>();
 
     public void addCommand(WaiterCommand command) {
         queue.add(command);
